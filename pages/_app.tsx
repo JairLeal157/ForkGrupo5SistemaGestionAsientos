31b2520dd45e4051c52e8949d23cf77665efc101
Jair Leal <jair.leal@udea.edu.co>
2024-05-16 17:59:23 -0500

    adapter y modelo prisma, provider auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps: { session, ...pageProps } }: any) => {
+  return (
+    <Component {...pageProps} />
+  );
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
+import { signIn } from "next-auth/react";
+
+
 const Home = () => {
-  return <main className='h-screen w-full flex items-center justify-center'>Hola mundo</main>;
+  return <main className='h-screen w-full flex items-center justify-center'>
+  <div
+    onClick={() => 
+      signIn('auth0')
+    }
+    className='h-[50px] w-[50px] rounded-full bg-[#8CFE68] cursor-pointer'>
+
+  </div>
+  </main>;
 };
 
 export default Home;
