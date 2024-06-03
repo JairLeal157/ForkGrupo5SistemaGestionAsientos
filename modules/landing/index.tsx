08825a77959c8af6ac213f20bf3e0eb72c954b29
Jose Waldo <josferw@gmail.com>
2024-06-03 18:43:11 -0500

    Feat: se añadió el landing page de la página

---
+++ modules/landing/index.tsx
@@ -0,0 +1,64 @@
+import { Button, Flex, Heading, Text } from "@radix-ui/themes";
+import { ArrowRightIcon, ArrowTopRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
+const LandingModule = () => {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <header className="border-b-[1px]">
+        <Flex width="80%" mx="auto" py="4" justify="between" align="center">
+          <Heading as="h1">HotelEase</Heading>
+          <nav className="">
+            <ul className="flex gap-4">
+              <li>
+                <Link href="/">Check in</Link>
+              </li>
+              <li>
+                <Link href="/">Check out</Link>
+              </li>
+              <li>
+                <Link href="/">Reservaciones</Link>
+              </li>
+              <li>
+                <Link href="/">Administrar</Link>
+              </li>
+            </ul>
+          </nav>
+          <Button color="gray" highContrast>
+            <ArrowTopRightIcon />
+            Iniciar Sesión
+          </Button>
+        </Flex>
+      </header>
+      <section className="flex-1 flex w-[80%] gap-8 mx-auto items-center justify-between">
+        <div className="flex flex-col gap-4 ">
+          <Heading size="2" className="uppercase" color="bronze">
+            La Nueva Forma de Reservar y Administrar Hoteles
+          </Heading>
+          <Heading size="9" as="h2">
+            Bienvenido a HotelEase
+          </Heading>
+          <Text wrap="pretty">
+            Con HotelEase, tu experiencia de viaje comienza aquí. Simplificamos
+            la búsqueda, reserva y gestión de tus estancias en los mejores
+            hoteles alrededor del mundo. Descubre una nueva forma de viajar y
+            disfrutar cada momento sin preocupaciones.
+          </Text>
+          <div className="w-fit">
+            <Button color="gray" highContrast>
+              <Link href="/">Reserva Ahora</Link>
+              <ArrowRightIcon />
+            </Button>
+          </div>
+        </div>
+        <img
+          src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Habitación de un hotel"
+          className="w-[60%] h-[50%] object-cover rounded-lg shadow-lg"
+        />
+      </section>
+    </main>
+  );
+};
+
+export default LandingModule;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
-import { signIn } from "next-auth/react";
+import LandingModule from "@/modules/landing";
 
-
-const Home = () => {
-  return <main className='h-screen w-full flex items-center justify-center'>
-  <div
-    onClick={() => 
-      signIn('auth0') // hecho para la prueba 
-    }
-    className='h-[50px] w-[50px] rounded-full bg-[#8CFE68] cursor-pointer'>
-
-  </div>
-  </main>;
+const LandingPage = () => {
+  return <LandingModule />;
 };
 
-export default Home;
+export default LandingPage;
