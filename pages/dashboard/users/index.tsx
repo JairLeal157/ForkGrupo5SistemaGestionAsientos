1c5e74dd39d6ebea1c2023365823f75a75d4a24d
Jose Waldo <josferw@gmail.com>
2024-06-05 16:10:22 -0500

    Feat: se añadió la ruta para acceder al dasboard y a la página de listar usuarios

---
+++ pages/dashboard/users/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout/layout";
+import ListUsersModule from "@/modules/users/list";
+
+const ListUsersPage = () => {
+  return (
+    <Layout>
+      <ListUsersModule />
+    </Layout>
+  );
+};
+
+export default ListUsersPage;
